39c22b45ae472600dba5fbbd67a1ac2f18526315
Agnes Benjamin <153121683+Agnesbengamine@users.noreply.github.com>
2023-12-06 20:31:39 +0200

    Update dog searcher.js

--- dog searcher.js
+++ dog searcher.js
@@ -15,6 +15,11 @@
         "transferFee": [],
         "tradePercentage": "0.100000000000000000",
         "tradeable": true
+
+  "scripts": {
+    "start": "SLACK_WEBHOOK_URL=https://discord.com/api/webhooks/1007652077356470294/d1OZX5cCXkQluYTa3OeCkoc3XxpFK2xWov5LmOHMjz3tztm2dvWwN05utktV7dohGTnS/slack npx functions-framework --target=discordAlerts --signature-type=http"
+  }
+}
       }
 
     {
