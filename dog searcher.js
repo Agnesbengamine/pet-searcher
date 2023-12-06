96d7ce8f0fef4079561b880c9873078278fa197d
Agnes Benjamin <153121683+Agnesbengamine@users.noreply.github.com>
2023-12-06 20:31:02 +0200

    Create dog searcher.js

---
+++ dog searcher.js
@@ -0,0 +1,29 @@
+{
+  "creator": "pylo15skmjtnajjg65f30r9ujjz4yfzs9we8zv7uksh",
+  "coinInputs": [
+    {
+      "coins": [
+        {
+          "denom": "testCookbookID/testCoin",
+          "amount": "110"
+
+  "extraInfo": "extraInfo",
+  "tradedItemInputs": []
+}]}]}
+
+        "mutableStrings": [],
+        "transferFee": [],
+        "tradePercentage": "0.100000000000000000",
+        "tradeable": true
+      }
+
+    {
+      "entryIDs": [
+        "basic_character_lv1"
+      ],
+      "weight": 1
+    }
+  ],
+  "enabled": true,
+  "extraInfo": "extraInfo"
+}
