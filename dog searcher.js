fb2622dbedafc968fb962d8234a5b002cfba8ea7
Agnes Benjamin <153121683+Agnesbengamine@users.noreply.github.com>
2023-12-06 20:32:15 +0200

    Update dog searcher.js

--- dog searcher.js
+++ dog searcher.js
@@ -20,6 +20,8 @@
     "start": "SLACK_WEBHOOK_URL=https://discord.com/api/webhooks/1007652077356470294/d1OZX5cCXkQluYTa3OeCkoc3XxpFK2xWov5LmOHMjz3tztm2dvWwN05utktV7dohGTnS/slack npx functions-framework --target=discordAlerts --signature-type=http"
   }
 }
+
+// some comments to find out whats happen here
       }
 
     {
